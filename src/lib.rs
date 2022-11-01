abf1a670101b2095bb4e6015796e99074211d85e
Richard Lincoln <r.w.lincoln@gmail.com>
2022-11-01 20:51:52 +0100

    Using Rayon to parallelize the solve function for multiple RHS.

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ use dfs::ludfs;
 use internal::*;
 use maxmatch::maxmatch;
 use num_traits::PrimInt;
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSliceMut;
 pub use rlu::*;
 pub use scalar::Scalar;
 use solve::*;
@@ -337,7 +339,11 @@ pub fn factor<I: PrimInt + Display, S: Scalar>(
 
 /// Solve `Ax=b` for one or more right-hand-sides given the numeric
 /// factorization of A from `factor`.
-pub fn solve<S: Scalar>(lu: &LU<S>, rhs: &mut [S], trans: bool) -> Result<(), String> {
+pub fn solve<S: Scalar + Send + Sync>(
+    lu: &LU<S>,
+    rhs: &mut [S],
+    trans: bool,
+) -> Result<(), String> {
     let n = lu.n;
     if rhs.len() % n != 0 {
         return Err(format!(
@@ -346,60 +352,58 @@ pub fn solve<S: Scalar>(lu: &LU<S>, rhs: &mut [S], trans: bool) -> Result<(), St
             n
         ));
     }
-    let nrhs = rhs.len() / n;
 
-    let mut work = vec![S::zero(); n];
-
-    for i in 0..nrhs {
-        let mut b = &mut rhs[i * n..i * n + n];
-        if !trans {
-            lsolve(
-                n,
-                &lu.lu_nz,
-                &lu.lu_row_ind,
-                &lu.l_col_ptr,
-                &lu.u_col_ptr,
-                &lu.row_perm,
-                &lu.col_perm,
-                &b,
-                &mut work,
-            )?;
-            usolve(
-                n,
-                &lu.lu_nz,
-                &lu.lu_row_ind,
-                &lu.l_col_ptr,
-                &lu.u_col_ptr,
-                &lu.row_perm,
-                &lu.col_perm,
-                &mut work,
-                &mut b,
-            )?;
-        } else {
-            utsolve(
-                n,
-                &lu.lu_nz,
-                &lu.lu_row_ind,
-                &lu.l_col_ptr,
-                &lu.u_col_ptr,
-                &lu.row_perm,
-                &lu.col_perm,
-                &b,
-                &mut work,
-            )?;
-            ltsolve(
-                n,
-                &lu.lu_nz,
-                &lu.lu_row_ind,
-                &lu.l_col_ptr,
-                &lu.u_col_ptr,
-                &lu.row_perm,
-                &lu.col_perm,
-                &mut work,
-                &mut b,
-            )?;
-        }
-    }
-
-    return Ok(());
+    rhs.par_chunks_exact_mut(n).try_for_each_with(
+        vec![S::zero(); n],
+        |mut work, mut b| -> Result<(), String> {
+            if !trans {
+                lsolve(
+                    n,
+                    &lu.lu_nz,
+                    &lu.lu_row_ind,
+                    &lu.l_col_ptr,
+                    &lu.u_col_ptr,
+                    &lu.row_perm,
+                    &lu.col_perm,
+                    &b,
+                    &mut work,
+                )?;
+                usolve(
+                    n,
+                    &lu.lu_nz,
+                    &lu.lu_row_ind,
+                    &lu.l_col_ptr,
+                    &lu.u_col_ptr,
+                    &lu.row_perm,
+                    &lu.col_perm,
+                    &mut work,
+                    &mut b,
+                )?;
+            } else {
+                utsolve(
+                    n,
+                    &lu.lu_nz,
+                    &lu.lu_row_ind,
+                    &lu.l_col_ptr,
+                    &lu.u_col_ptr,
+                    &lu.row_perm,
+                    &lu.col_perm,
+                    &b,
+                    &mut work,
+                )?;
+                ltsolve(
+                    n,
+                    &lu.lu_nz,
+                    &lu.lu_row_ind,
+                    &lu.l_col_ptr,
+                    &lu.u_col_ptr,
+                    &lu.row_perm,
+                    &lu.col_perm,
+                    &mut work,
+                    &mut b,
+                )?;
+            }
+            return Ok(());
+        },
+    )
 }
