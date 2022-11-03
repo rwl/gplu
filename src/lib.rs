ed8a568a6bd8204d291fb8b04242bd5a82bb8fe0
Richard Lincoln <r.w.lincoln@gmail.com>
2022-11-03 13:30:55 +0100

    Reinstating sequential solve function for use when the overhead of creating threads outweighs the benefits of parallelism.
    Renaming parallel solve function as `par_solve`.

--- src/lib.rs
+++ src/lib.rs
@@ -339,7 +339,76 @@ pub fn factor<I: PrimInt + Display, S: Scalar>(
 
 /// Solve `Ax=b` for one or more right-hand-sides given the numeric
 /// factorization of A from `factor`.
-pub fn solve<S: Scalar + Send + Sync>(
+pub fn solve<S: Scalar>(lu: &LU<S>, rhs: &mut [S], trans: bool) -> Result<(), String> {
+    let n = lu.n;
+    if rhs.len() % n != 0 {
+        return Err(format!(
+            "len rhs ({}) must be a multiple of n ({})",
+            rhs.len(),
+            n
+        ));
+    }
+    let nrhs = rhs.len() / n;
+
+    let mut work = vec![S::zero(); n];
+
+    for i in 0..nrhs {
+        let mut b = &mut rhs[i * n..i * n + n];
+        if !trans {
+            lsolve(
+                n,
+                &lu.lu_nz,
+                &lu.lu_row_ind,
+                &lu.l_col_ptr,
+                &lu.u_col_ptr,
+                &lu.row_perm,
+                &lu.col_perm,
+                &b,
+                &mut work,
+            )?;
+            usolve(
+                n,
+                &lu.lu_nz,
+                &lu.lu_row_ind,
+                &lu.l_col_ptr,
+                &lu.u_col_ptr,
+                &lu.row_perm,
+                &lu.col_perm,
+                &mut work,
+                &mut b,
+            )?;
+        } else {
+            utsolve(
+                n,
+                &lu.lu_nz,
+                &lu.lu_row_ind,
+                &lu.l_col_ptr,
+                &lu.u_col_ptr,
+                &lu.row_perm,
+                &lu.col_perm,
+                &b,
+                &mut work,
+            )?;
+            ltsolve(
+                n,
+                &lu.lu_nz,
+                &lu.lu_row_ind,
+                &lu.l_col_ptr,
+                &lu.u_col_ptr,
+                &lu.row_perm,
+                &lu.col_perm,
+                &mut work,
+                &mut b,
+            )?;
+        }
+    }
+
+    return Ok(());
+}
+
+/// Solve `Ax=b` for one or more right-hand-sides given the numeric
+/// factorization of A from `factor`.
+pub fn par_solve<S: Scalar + Send + Sync>(
     lu: &LU<S>,
     rhs: &mut [S],
     trans: bool,
